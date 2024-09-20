44184c479b7922256456b4ae965b72feab1c8494
thelastkiss113 <76501819+thelastkiss113@users.noreply.github.com>
2024-09-20 09:45:36 -0700

    added try/catch to test function

--- index.js
+++ index.js
@@ -99,11 +99,56 @@ const CourseInfo = {
   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
   
   console.log(result);
+
+  //Console Output : 
+//   [
+//     {
+//       id: 125,
+//       avg: 0.985, // (47 + 150) / (50 + 150)
+//       1: 0.94,    // 47 / 50
+//       2: 1.0      // 150 / 150
+//     },
+//     {
+//       id: 132,
+//       avg: 0.82,  // (39 + 125) / (50 + 150)
+//       1: 0.78,    // 39 / 50
+//       2: 0.833    // late: (140 - 15) / 150
+//     }
+//   ]
   
 
 // Function: make sure AssignmentGroup.course_id matches CourseInfo.id ==> if it doesn't throw error
-function  assignmentGroupMatch(course, AssignmentGroup) {
-if (AssignmentGroup.course_id !== CourseInfo.id) {
-    throw new Error ("Assignment group does not match course");
+function assignmentGroupMatch(course, AssignmentGroup) {
+if (AssignmentGroup.course_id !== course.id) {
+    throw new Error("Assignment group does not match course");
+  } else {
+    console.log("Assignment group valid");
   }
 }
+
+// Test the function
+try {
+    assignmentGroupMatch(CourseInfo, AssignmentGroup); 
+  } catch (error) {
+    console.error(error.message);
+  }
+  //console Output : Assignment group valid.
+
+// Validate AssignmentInfo
+
+
+//Checklist:
+// Declare variables properly using let and const where appropriate.
+// Use operators to perform calculations on variables and literals.
+// Use strings, numbers, and Boolean values cached within variables.
+// Use at least two if/else statements to control program flow. Optionally, use at least one switch statement.
+// Use try/catch statements to manage potential errors in the code, such as incorrectly formatted or typed data being fed into your program.
+// Utilize at least two different types of loops.
+// Utilize at least one loop control keyword such as break or continue.
+// Create and/or manipulate arrays and objects.
+// Demonstrate the retrieval, manipulation, and removal of items in an array or properties in an object.
+// Use functions to handle repeated tasks.
+// Program outputs processed data as described above. Partial credit will be earned depending on the level of adherence to the described behavior.
+// Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
+// Commit frequently to the git repository.
+// Include a README file that contains a description of your application.
